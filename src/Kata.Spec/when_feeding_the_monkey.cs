62668f92ed2c8fbcab86a9c166436cc5036762c1
csolorzanof <carlos.solorzano@acklenavenue.com>
2019-08-01 10:42:46 -0600

    Pong

--- src/Kata.Spec/when_feeding_the_monkey.cs
+++ src/Kata.Spec/when_feeding_the_monkey.cs
@@ -26,12 +26,26 @@ namespace Kata.Spec
 
         Because of = () => { _result = _systemUnderTest.Add(); };
 
-        It should_return_zero = () => { _result.Should().Be(1); };
+        It should_return_zero = () => { _result.Should().Be(0); };
         static Calculator _systemUnderTest;
         static int _result;
     }
 
-    //    1. Given the user input is empty when calculating the sum then it should return zero.
+    public class when_input_is_one_number
+    {
+        Establish _context = () =>
+        {
+            _systemUnderTest = new Calculator();
+        };
+
+        Because of = () => { _result = _systemUnderTest.Add("1"); };
+
+        It should_return_the_same_number = () => { _result.Should().Be(1); };
+        static Calculator _systemUnderTest;
+        static int _result;
+    }
+
+    
 //    2. Given the user input is one number when calculating the sum then it should return the same number. (example "3" should equal 3)
 //    3. Given the user input is two numbers when calculating the sum then it should return the sum of those numbers. (example "1,2" should equal 3)
 //    4. Given the user input is an unknown amount of numbers when calculating the sum then it should return the sum of all the numbers. (example "1,2,3" should equal 6)
