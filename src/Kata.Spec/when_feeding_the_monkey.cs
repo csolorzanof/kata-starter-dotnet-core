5b21add05b485a865a6cfb82f90d3148f00b30f6
Byron Sommardahl <byron@acklenavenue.com>
2019-08-02 10:10:40 -0500

    Ping

--- src/Kata.Spec/when_feeding_the_monkey.cs
+++ src/Kata.Spec/when_feeding_the_monkey.cs
@@ -6,11 +6,11 @@ namespace Kata.Spec
     public class when_feeding_the_monkey
     {
         static Monkey _systemUnderTest;
-        
-        Establish context = () => 
+
+        Establish context = () =>
             _systemUnderTest = new Monkey();
 
-        Because of = () => 
+        Because of = () =>
             _systemUnderTest.Eat("banana");
 
         It should_have_the_food_in_its_belly = () =>
@@ -19,10 +19,7 @@ namespace Kata.Spec
 
     public class when_input_is_empty
     {
-        Establish _context = () =>
-        {
-            _systemUnderTest = new Calculator();
-        };
+        Establish _context = () => { _systemUnderTest = new Calculator(); };
 
         Because of = () => { _result = _systemUnderTest.Add(); };
 
@@ -31,8 +28,17 @@ namespace Kata.Spec
         static int _result;
     }
 
+    public class when_user_input_is_only_one_number
+    {
+        Establish _context = () => { _systemUnderTest = new Calculator(); };
 
-//    1. Given the user input is empty when calculating the sum then it should return zero.
+        Because of = () => { _result = _systemUnderTest.Add("1"); };
+
+        It should_return_the_same_number = () => { _result.Should().Be(1); };
+        static Calculator _systemUnderTest;
+        static int _result;
+    }
+
 //    2. Given the user input is one number when calculating the sum then it should return the same number. (example "3" should equal 3)
 //    3. Given the user input is two numbers when calculating the sum then it should return the sum of those numbers. (example "1,2" should equal 3)
 //    4. Given the user input is an unknown amount of numbers when calculating the sum then it should return the sum of all the numbers. (example "1,2,3" should equal 6)
@@ -43,5 +49,4 @@ namespace Kata.Spec
 //    9. Given the user input contains numbers larger than 1000 when calculating the sum it should only sum the numbers less than 1001. (example 2 + 1001 = 2)
 //    10. Given the user input is multiple numbers with a custom multi-character delimiter when calculating the sum then it should return the sum of all the numbers. (example: “//[***]\n1***2***3” should return 6)
 //    11. Given the user input is multiple numbers with multiple custom delimiters when calculating the sum then it should return the sum of all the numbers. (example “//[*][%]\n1*2%3” should return 6)
-
 }
