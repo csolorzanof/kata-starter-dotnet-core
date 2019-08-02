ede11d6dbf9797269e256e3639293ea6268f29d8
Byron Sommardahl <byron@acklenavenue.com>
2019-08-02 10:39:54 -0500

    Ping

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -5,26 +5,28 @@ namespace Kata
 {
     public class Calculator
     {
-        public int Add(string s="")
+        public int Add(string s = "")
         {
-            if(string.IsNullOrEmpty(s))
+            if (string.IsNullOrEmpty(s))
                 return 0;
 
-            string[] delimeters = new[] {",", "\n"};
+            var delimeters = new[] {",", "\n"};
             if (s.StartsWith("//"))
             {
-                string[] components = s.Split("\n");
+                var components = s.Split("\n");
                 delimeters = new[] {components[0].Replace("//", "")};
                 s = components[1];
             }
-            
-            var numbers = s.Split(delimeters, StringSplitOptions.None).Select(int.Parse).ToArray();
 
-            var negatives = numbers.Where(x => x < 0).ToArray();
-            if (negatives.Any())
-            {
-                throw new Exception($"negatives not allowed: {string.Join(", ",negatives)}");
-            }
+            var numbers = s.Split(delimeters, StringSplitOptions.None)
+                .Select(int.Parse)
+                .Where(x => x < 1001)
+                .ToArray();
+
+            var negatives = numbers
+                .Where(x => x < 0)
+                .ToArray();
+            if (negatives.Any()) throw new Exception($"negatives not allowed: {string.Join(", ", negatives)}");
 
             return numbers.Sum();
         }
