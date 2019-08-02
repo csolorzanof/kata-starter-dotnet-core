70e6378dc6af5969d711921dcee2a2d03623603b
csolorzanof <carlos.solorzano@acklenavenue.com>
2019-08-02 09:28:54 -0600

    Ping

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -10,7 +10,15 @@ namespace Kata
             if(string.IsNullOrEmpty(s))
                 return 0;
 
-            var numbers = s.Split(new []{",","\n"}, StringSplitOptions.None).Select(int.Parse).ToArray();
+            string[] delimeters = new[] {",", "\n"};
+            if (s.StartsWith("//"))
+            {
+                string[] components = s.Split("\n");
+                delimeters = new[] {components[0].Replace("//", "")};
+                s = components[1];
+            }
+            
+            var numbers = s.Split(delimeters, StringSplitOptions.None).Select(int.Parse).ToArray();
 
             return numbers.Sum();
         }
