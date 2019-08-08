91668c3f5681c928dada8c32418ea6a985a5844e
csolorzanof <carlos.solorzano@acklenavenue.com>
2019-08-08 10:31:29 -0600

    Ping

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -9,7 +9,15 @@ namespace Kata
         {
             if (string.IsNullOrEmpty(s))
                 return 0;
-            var array = s.Split(new []{",", "\n"}, StringSplitOptions.None).Select(int.Parse).ToArray();
+            
+            var separator = new []{",", "\n"};
+            if (s.StartsWith("//"))
+            {
+                separator = new[] {s.Split("\n")[0].Replace("//", "")};
+                s = s.Split("\n")[1];
+            }
+                        
+            var array = s.Split(separator, StringSplitOptions.None).Select(int.Parse).ToArray();
             if (array.Length == 1)
                 return array.First();
 
