49ed5e44b51ea635896067628df010e1f627d714
csolorzanof <carlos.solorzano@acklenavenue.com>
2019-08-15 12:51:21 -0600

    Ping

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -1,10 +1,18 @@
+using System.Linq;
+
 namespace Kata
 {
     public class Calculator
     {
         public int Add(string s = "")
         {
-            return 0;
+            if (string.IsNullOrEmpty(s))
+                return 0;
+
+            var numbers = s.Split(",").Select(x=> int.Parse(x)).ToArray();
+            if (numbers.Length == 1)
+                return numbers.First();
+            return numbers.First() + numbers.Last();
         }
     }
 }
