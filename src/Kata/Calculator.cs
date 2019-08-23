4fe6139ab520a125fed3a7094ad9427fc34785c7
csolorzanof <carlos.solorzano@acklenavenue.com>
2019-08-23 09:42:32 -0600

    multiple numbers passing, newline failing

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -10,10 +10,8 @@ namespace Kata
                 return 0;
 
             var numbers = s.Split(",").Select(int.Parse);
-            if(numbers.Count()==1)
-                return 1;
-
-            return numbers.First() + numbers.Last();
+            
+            return numbers.Sum();
         }
     }
 }
